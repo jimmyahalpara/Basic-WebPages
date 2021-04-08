7887407dbec626de35c7230825441f48cea513bd
JimmyAhalpara <jimmyahalpara123@gmail.com>
2021-04-08 13:55:40 +0530

    learning node js

---
+++ 55. Dance Website/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+// const fs = require("fs");
+const app = express();
+const port = 8000;
+
+// express specific stuff
+app.use("/static",express.static("static"));
+app.use(express.urlencoded());
+
+
+// PUG specific stuff
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+// Endpoints 
+app.get('/', (req, res)=> {
+    const parms = {};
+    res.status(200).render("index.pug", parms);
+})
+
+app.listen(port, ()=>{
+    console.log(`The application started sucessfully on port ${port}`);
+})
